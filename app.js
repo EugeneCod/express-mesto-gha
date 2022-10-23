865957c619c7d91f46a7cd06239cee4a9629a256
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-10-23 17:05:54 +0300

    feat: add celevrate to routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const console = require('console');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -21,8 +22,22 @@ app.use(express.json());
 
 mongoose.connect(MONGO_URL);
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+  }),
+}), createUser);
 
 // авторизация
 app.use(auth);
@@ -34,7 +49,10 @@ app.use('*', (req, res, next) => {
   next(new NotFoundError(ERROR_MESSAGES.INVALID_ADDRESS_OR_METHOD));
 });
 
-// обработка ошибок
+// обработчик ошибок celebrate
+app.use(errors());
+
+// централизованный обработчик ошибок
 app.use(errorHandler);
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers,
   getUser,
@@ -9,8 +10,24 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getAuthorizedUser);
-router.get('/:userId', getUser);
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateUserAvatar);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), getUser);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUserInfo);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), updateUserAvatar);
 
 module.exports = router;
